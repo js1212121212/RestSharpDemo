551b83fea7f7b02d915e0d27e0741f58fc47534e
Jennifer Baik <sbaik058@gmail.com>
2020-01-26 14:23:11 +1300

    Simple GET testing

---
+++ RestSharpDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestSharpDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RestSharpDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eb095123-b002-4671-9c88-c8fff0a499b1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RestSharpDemo/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+
+namespace RestSharpDemo {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+            var client = new RestClient("http://localhost:3000/");
+            //will be passing post/1
+            var request = new RestRequest("posts/{postid}", Method.GET);
+            request.AddUrlSegment("postid", 1);
+            var content = client.Execute(request).Content;
+        }
+    }
+}
