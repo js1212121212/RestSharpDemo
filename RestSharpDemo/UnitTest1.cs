47d4c0ef2a774c0facf6869f9605ff1b25e5478c
Jennifer Baik <sbaik058@gmail.com>
2020-01-28 23:57:59 +1300

    How to send a simple request and use NUnit and Newtonsoft

--- RestSharpDemo/UnitTest1.cs
+++ RestSharpDemo/UnitTest1.cs
@@ -1,17 +1,29 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 using RestSharp;
+using RestSharp.Serialization.Json;
+using Newtonsoft.Json.Linq;
 
 namespace RestSharpDemo {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1 {
-        [TestMethod]
+        [Test]
         public void TestMethod1() {
             var client = new RestClient("http://localhost:3000/");
             //will be passing post/1
             var request = new RestRequest("posts/{postid}", Method.GET);
             request.AddUrlSegment("postid", 1);
-            var content = client.Execute(request).Content;
+            var response = client.Execute(request);
+
+            //using Newtonsoft
+            JObject obs = JObject.Parse(response.Content);
+
+            //below is without using Newtonsoft
+            //var deserialise = new JsonDeserializer();
+            //var output = deserialise.Deserialize<Dictionary<string, string>>(response);
+            //var writer = output["author"];
+
+            Assert.That(obs["author"].ToString(), Is.EqualTo("George BB"), "Author is not correct");
         }
     }
 }
